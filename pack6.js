f6e7aaabc910b5218d382f653427ca11b07f9d03
ujwalpandey012 <ujwalpandey012@gmail.com>
2025-11-17 11:33:29 +1100

    Update pack6.js

--- pack6.js
+++ pack6.js
@@ -59,7 +59,7 @@ const pack6 = [
     options: [
       "Chinese Empire<br>चिनियाँ साम्राज्य",
       "Roman Empire<br>रोमन साम्राज्य",
-      "Ottoman Empire<br>अटोमन साम्राज्य",
+      "Ottoman Empire<br>अोटोमन साम्राज्य",
       "Persian Empire<br>पर्सियन साम्राज्य"
     ],
     correct: "Chinese Empire<br>चिनियाँ साम्राज्य"
@@ -107,5 +107,117 @@ const pack6 = [
       "Sarnath<br>सारनाथ"
     ],
     correct: "Lumbini<br>लुम्बिनी"
+  },
+
+  // ================= HARDER 10 QUESTIONS ================= //
+
+  {
+    q: "Who was the first emperor of the Maurya Empire?<br>मौर्य साम्राज्यका पहिलो सम्राट् को थिए?",
+    options: [
+      "Chandragupta Maurya<br>चन्द्रगुप्त मौर्य",
+      "Ashoka<br>अशोक",
+      "Bindusara<br>बिन्दुसार",
+      "Harsha<br>हर्ष"
+    ],
+    correct: "Chandragupta Maurya<br>चन्द्रगुप्त मौर्य"
+  },
+
+  {
+    q: "Which civilization built Machu Picchu?<br>माचू पिच्चू कसले निर्माण गर्यो?",
+    options: [
+      "Inca Civilization<br>इन्का सभ्यता",
+      "Mayan Civilization<br>मायान सभ्यता",
+      "Aztec Civilization<br>एज्टेक सभ्यता",
+      "Greek Civilization<br>ग्रीक सभ्यता"
+    ],
+    correct: "Inca Civilization<br>इन्का सभ्यता"
+  },
+
+  {
+    q: "Who was the first woman to fly solo across the Atlantic Ocean?<br>एटलान्टिक महासागर एक्लै उडान भर्ने पहिलो महिला को थिइन्?",
+    options: [
+      "Amelia Earhart<br>अमेलिया इअरहर्ट",
+      "Valentina Tereshkova<br>भ्यालेन्टिना तेरेश्कोभा",
+      "Marie Curie<br>मेरी क्युरी",
+      "Joan of Arc<br>जोआन अफ अर्क"
+    ],
+    correct: "Amelia Earhart<br>अमेलिया इअरहर्ट"
+  },
+
+  {
+    q: "Which ancient city was buried under volcanic ash in 79 AD?<br>७९ ईस्वीमा ज्वालामुखीको खरानीले पुरिएको शहर कुन हो?",
+    options: [
+      "Pompeii<br>पोम्पेई",
+      "Alexandria<br>अलेक्जान्द्रिया",
+      "Babylon<br>बेबिलोन",
+      "Troy<br>ट्रोय"
+    ],
+    correct: "Pompeii<br>पोम्पेई"
+  },
+
+  {
+    q: "Who was the longest-reigning monarch in British history?<br>ब्रिटेनको इतिहासमा सबैभन्दा लामो शासन गर्ने सम्राट् को हुन्?",
+    options: [
+      "Queen Elizabeth II<br>क्वीन एलिजाबेथ द्वितीय",
+      "Queen Victoria<br>क्वीन भिक्टोरिया",
+      "King Charles III<br>राजा चार्ल्स तृतीय",
+      "Edward VII<br>एडवर्ड सातौँ"
+    ],
+    correct: "Queen Elizabeth II<br>क्वीन एलिजाबेथ द्वितीय"
+  },
+
+  {
+    q: "Which explorer first circumnavigated the Earth?<br>पृथ्वी पहिलो पटक कसले परिक्रमा गरे?",
+    options: [
+      "Ferdinand Magellan<br>फर्डिनान्ड म्यागेलन",
+      "Marco Polo<br>मार्को पोलो",
+      "Vasco da Gama<br>वास्को द गामा",
+      "James Cook<br>जेम्स कुक"
+    ],
+    correct: "Ferdinand Magellan<br>फर्डिनान्ड म्यागेलन"
+  },
+
+  {
+    q: "Which revolution began in 1760 affecting manufacturing worldwide?<br>उत्पादन क्षेत्रमा ठूलो परिवर्तन ल्याउने १७६० को क्रान्ति कुन हो?",
+    options: [
+      "Industrial Revolution<br>औद्योगिक क्रान्ति",
+      "French Revolution<br>फ्रेन्च क्रान्ति",
+      "American Revolution<br>अमेरिकन क्रान्ति",
+      "Scientific Revolution<br>वैज्ञानिक क्रान्ति"
+    ],
+    correct: "Industrial Revolution<br>औद्योगिक क्रान्ति"
+  },
+
+  {
+    q: "Where was the ancient city of Babylon located?<br>बेबिलोन प्राचीन शहर कहाँ थियो?",
+    options: [
+      "Iraq<br>इराक",
+      "Egypt<br>इजिप्ट",
+      "Iran<br>इरान",
+      "Turkey<br>टर्की"
+    ],
+    correct: "Iraq<br>इराक"
+  },
+
+  {
+    q: "Who taught Alexander the Great?<br>अलेक्जान्डर महानलाई कसले पढाए?",
+    options: [
+      "Aristotle<br>एरिस्टोटल",
+      "Plato<br>प्लेटो",
+      "Socrates<br>सक्रेटिस",
+      "Heraclitus<br>हेराक्लाइटस"
+    ],
+    correct: "Aristotle<br>एरिस्टोटल"
+  },
+
+  {
+    q: "Which ancient wonder was located in Egypt?<br>प्राचीन सात आश्चर्यमध्ये कुन इजिप्टमा थियो?",
+    options: [
+      "Great Pyramid of Giza<br>गिजाको महान पिरामिड",
+      "Hanging Gardens of Babylon<br>बेबिलोनका झुन्डिने बगैँचा",
+      "Temple of Artemis<br>आर्टेमिसको मन्दिर",
+      "Statue of Zeus<br>जियसको मूर्ति"
+    ],
+    correct: "Great Pyramid of Giza<br>गिजाको महान पिरामिड"
   }
 ];
